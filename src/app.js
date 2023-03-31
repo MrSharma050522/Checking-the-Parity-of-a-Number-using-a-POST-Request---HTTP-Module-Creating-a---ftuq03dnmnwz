a6188b7b8c2919d2b510faff16a18f80ef75d1a0
Sandeep Sharma <sandeepsharma10697@gmail.com>
2023-03-31 20:46:54 +0530

    done

--- src/app.js
+++ src/app.js
@@ -1,23 +1,28 @@
-const http = require('http');
+const http = require("http");
 
 const server = http.createServer((req, res) => {
-  if (req.method === 'POST') {
+  if (req.method === "POST") {
     const chunks = [];
 
-    req.on('data', chunk => {
+    req.on("data", (chunk) => {
       const buf = Buffer.from(chunk);
       const str = buf.toString();
       chunks.push(str);
-      const obj = JSON.parse(chunks)
+      const obj = JSON.parse(chunks);
       const value = obj.num1;
-    
-     // Write the code here to check if the number is odd or even
+      if (value % 2 === 0) {
+        // res.statusCode = 200;
+        res.writeHead(200, { "Content-Type": "text/plain" });
+        res.end(
+          JSON.stringify({
+            body: `The number ${value} is even`,
+          })
+        );
+      }
 
-   });
+      // Write the code here to check if the number is odd or even
+    });
   }
-
-  
 });
-
 
 module.exports = server;
