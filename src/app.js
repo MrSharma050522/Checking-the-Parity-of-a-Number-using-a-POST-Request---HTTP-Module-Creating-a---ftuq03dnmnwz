62a33c3872017b03436f85296699dc38ed0a1200
Sandeep Sharma <sandeepsharma10697@gmail.com>
2023-04-01 15:14:33 +0530

    changes

--- src/app.js
+++ src/app.js
@@ -11,32 +11,17 @@ const server = http.createServer((req, res) => {
       const obj = JSON.parse(chunks);
       const value = obj.num1;
       if (isNaN(value)) {
-        res.statusCode = 400;
-        // res.writeHead({ "Content-Type": "text/plain" });
-        res.end(
-          JSON.stringify({
-            "Content-Type": "text/plain",
-            body: `${value} is not a number`,
-          })
-        );
+        // res.statusCode = 400;
+        res.writeHead(400, { "Content-Type": "text/plain" });
+        res.end(`${value} is not a number`);
       } else if (value % 2 === 0) {
-        res.statusCode = 200;
-        // res.writeHead({ "Content-Type": "text/plain" });
-        res.end(
-          JSON.stringify({
-            "Content-Type": "text/plain",
-            body: `The number ${value} is even`,
-          })
-        );
+        // res.statusCode = 200;
+        res.writeHead(200, { "Content-Type": "text/plain" });
+        res.end(`The number ${value} is even`);
       } else if (value % 2 !== 0) {
-        res.statusCode = 404;
-        // res.writeHead({ "Content-Type": "text/plain" });
-        res.end(
-          JSON.stringify({
-            "Content-Type": "text/plain",
-            body: `The number ${value} is odd`,
-          })
-        );
+        // res.statusCode = 404;
+        res.writeHead(404, { "Content-Type": "text/plain" });
+        res.end(`The number ${value} is odd`);
       }
 
       // Write the code here to check if the number is odd or even
